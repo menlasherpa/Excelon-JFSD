d715a3dd23fe474492268fb346207db3d1c0ecd3
Lakpa <lsherpa@miu.edu>
2024-01-08 15:59:02 -0600

    Day 4 commit again

---
+++ JSFD/src/com/excelon/app/day4/example/InstanceVariableDemo.java
@@ -0,0 +1,41 @@
+package com.excelon.app.day4.example;
+
+public class InstanceVariableDemo {
+	
+	// instance variables
+	int x = 101;
+	String name = "Menlaa";
+	
+	
+	// default constructor
+	public InstanceVariableDemo() {
+		System.out.println("Default constructor.....");
+	}
+	
+	// instance method
+	void show() {
+		System.out.println("Instance method..........");
+	}
+	
+	// static method
+	static void display() {
+		System.out.println("Static method...........");
+	}
+
+
+	public static void main(String[] args) {
+		
+		// to use instance variable and methods, we need object
+		InstanceVariableDemo id = new InstanceVariableDemo();
+		InstanceVariableDemo id1 = new InstanceVariableDemo();
+		id.show();
+		System.out.println("Number " + id.x);
+		id.x = 102;
+		System.out.println("Number with id object " + id.x);
+		System.out.println("Number with id1 object " + id1.x);
+		System.out.println("Name " + id.name);
+		
+		display();
+	}
+	
+}
---
+++ JSFD/src/com/excelon/app/day4/example/test/EmployeeManagerTest.java
@@ -0,0 +1,12 @@
+package com.excelon.app.day4.example.test;
+
+import com.excelon.app.day4.example.inheritance.Manager;
+
+public class EmployeeManagerTest {
+	
+	public static void main(String[] args) {
+		Manager m = new Manager("Manager");
+		System.out.println(m);
+	}
+	
+}
